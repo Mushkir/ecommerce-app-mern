35986e361f5308d3af7a9e51e2abe0d600d30c24
Mushkir <mushkirmohamed@gmail.com>
2024-09-21 02:16:21 +0530

    style & feat: designed signup page and implemented img upload feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,16 @@ import { Outlet } from "react-router-dom";
 import TheNavBar from "./components/TheNavBar";
 import TheFooter from "./components/TheFooter";
 
-function App() {
+const App = () => {
   return (
     <>
       <TheNavBar />
-      <main className="min-h-[calc(100vh-150px)]">
+      <main className="min-h-[calc(100vh-120px)]">
         <Outlet />
       </main>
       <TheFooter />
     </>
   );
-}
+};
 
 export default App;
