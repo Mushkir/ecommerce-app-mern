559db509fd5f2c513fdf3365fb55f0c0dc7c0172
Mushkir <mushkirmohamed@gmail.com>
2024-09-21 23:02:12 +0530

    feat: implemented db connection to backend

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,1 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("MongoDB is connected...");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
--- server/main.js
+++ server/main.js
@@ -1,11 +1,15 @@
 import express from "express";
 import router from "./routes/index.route.js";
+import connectDB from "./lib/db.js";
 
 const app = express();
 const PORT = 8080;
 
+app.use(express.json());
+
 app.use("/", router);
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running in http://localhost:${PORT}`);
 });
