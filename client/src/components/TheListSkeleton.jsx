6613c76d07c514b23294fffbab86ed8b5f0d0c67
Mushkir <mushkirmohamed@gmail.com>
2024-11-26 19:21:59 +0530

    feat: implemented show product detail by id feature

--- client/src/components/TheListSkeleton.jsx
+++ client/src/components/TheListSkeleton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const TheListSkeleton = ({ listsToRender, content }) => {
+const TheListSkeleton = ({ listsToRender = 1, content }) => {
   //   console.log(content);
 
   return (
@@ -17,7 +17,7 @@ const TheListSkeleton = ({ listsToRender, content }) => {
 };
 
 TheListSkeleton.propTypes = {
-  listsToRender: PropTypes.number.isRequired,
+  listsToRender: PropTypes.number,
   content: PropTypes.object.isRequired,
 };
 
--- client/src/pages/TheShowProductDetail.jsx
+++ client/src/pages/TheShowProductDetail.jsx
@@ -2,15 +2,20 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import currencyFormat from "../utils/currencyFormat";
 import apiEndPointObj from "../common/api_uri";
+import TheListSkeleton from "../components/TheListSkeleton";
 
 const TheShowProductDetail = () => {
   const { id } = useParams();
 
   const [productDetail, setProductDetail] = useState({});
+  const [productImgs, setProductImgs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [activeImg, setActiveImg] = useState(null);
   // console.log(id);
 
   const getProductDetail = async () => {
     try {
+      setLoading(true);
       const response = await fetch(
         apiEndPointObj.getProductDetailById.url + `/${id}`,
         {
@@ -18,10 +23,11 @@ const TheShowProductDetail = () => {
           credentials: "include",
         }
       );
-
+      setLoading(false);
       const respData = await response.json();
       if (!respData.error) {
         setProductDetail(respData.data[0]);
+        setProductImgs(respData.data[0].productImgs);
         // console.log(respData);
       }
     } catch (error) {
@@ -29,104 +35,148 @@ const TheShowProductDetail = () => {
     }
   };
 
-  console.log(productDetail);
-
   useEffect(() => {
     getProductDetail();
   }, []);
+
+  console.log(productDetail);
 
   return (
     <div className=" container mx-auto p-4 flex flex-col md:flex-row gap-3 font-Sen">
       {/* Product Imgs */}
       <div className="p-2 flex flex-col-reverse md:flex-row gap-3 w-full max-w-md">
         <div className=" flex flex-row md:flex-col gap-3">
-          {[1, 2, 3, 4].map((el) => {
-            return (
-              <div className=" rounded overflow-hidden" key={el}>
-                <img
-                  src="https://www.ama-assn.org/sites/ama-assn.org/files/styles/related_article_stub_image_1200x800_3_2/public/2024-04/2024-03-19-CARESIGNAL_1170x780px.png?itok=2VfMKhBy"
-                  alt="img"
-                  className="w-[5rem] h-[5rem] object-cover rounded"
-                />
-              </div>
-            );
-          })}
+          {loading ? (
+            <TheListSkeleton
+              listsToRender={4}
+              content={
+                <div className="overflow-hidden animate-pulse w-[5rem] h-[5rem] object-cover rounded bg-slate-200"></div>
+              }
+            />
+          ) : (
+            productImgs?.map((imgUrl, index) => {
+              return (
+                <div className=" rounded overflow-hidden" key={index}>
+                  <img
+                    src={imgUrl}
+                    alt="img"
+                    className="w-[5rem] h-[5rem] object-cover rounded mix-blend-multiply bg-slate-200"
+                  />
+                </div>
+              );
+            })
+          )}
         </div>
 
         {/* Main Img */}
-        <div>
-          <img
-            src="https://www.ama-assn.org/sites/ama-assn.org/files/styles/related_article_stub_image_1200x800_3_2/public/2024-04/2024-03-19-CARESIGNAL_1170x780px.png?itok=2VfMKhBy"
-            alt="img"
-            className="w-[360px] h-[360px] object-cover"
-          />
+        <div className=" bg-slate-200 w-[360px] h-[360px]">
+          {loading ? (
+            <TheListSkeleton
+              content={
+                <div className="w-full h-full bg-slate-200 animate-pulse"></div>
+              }
+            />
+          ) : (
+            <img
+              src={productImgs[0]}
+              alt="img"
+              className="w-full h-full object-scale-down mix-blend-multiply"
+            />
+          )}
         </div>
       </div>
 
       {/* Product Detail */}
       <div className="w-full py-2">
-        {/* Brand Name */}
-        <span className=" bg-red-200 text-red-700 px-2 py-1 rounded-full">
-          Boat
-        </span>
+        {loading ? (
+          <TheListSkeleton
+            content={
+              <div className="px-3">
+                {/* Brand Name */}
+                <span className="rounded-full bg-slate-200 px-10 py-1 animate-pulse"></span>
 
-        {/* Product name */}
-        <h3 className=" mt-2 text-4xl font-semibold">Airpods 111</h3>
+                {/* Product name */}
+                <h3 className=" my-2 text-4xl font-semibold px-20 py-5 rounded-full bg-slate-200 animate-pulse w-52"></h3>
 
-        {/* Category */}
-        <small className=" text-slate-400">Airpods</small>
+                {/* Category */}
+                <small className=" bg-slate-200 px-10 animate-pulse py-1 rounded-full"></small>
 
-        {/* Price */}
-        <div className=" flex items-center gap-2">
-          {/* Selling Price */}
-          <h2 className=" text-2xl font-semibold text-red-600">
-            {currencyFormat(999)}
-          </h2>
+                {/* Price */}
+                <div className=" flex items-center gap-2 mt-4">
+                  {/* Selling Price */}
+                  <h2 className=" text-2xl font-semibold bg-slate-200 px-20 py-5 rounded-full animate-pulse">
+                    {/* {currencyFormat(999)} */}
+                  </h2>
 
-          {/* Original price */}
-          <h2 className=" text-2xl font-semibold line-through text-slate-400">
-            {currencyFormat(999)}
-          </h2>
-        </div>
+                  {/* Original price */}
+                  <h2 className=" text-2xl font-semibold line-through bg-slate-200 px-20 py-5 rounded-full animate-pulse">
+                    {/* {currencyFormat(999)} */}
+                  </h2>
+                </div>
 
-        {/* Buttons */}
-        <div className=" flex items-center gap-4 mt-3 mb-3">
-          <button className=" px-5 min-w-24 border border-red-600 rounded py-1 text-red-600 hover:bg-red-600 hover:text-white transition-all">
-            Buy
-          </button>
-          <button className=" px-5 min-w-24 bg-red-600 border border-red-600 rounded py-1 text-white hover:bg-white hover:text-red-600 transition-all">
-            Add To Cart
-          </button>
-        </div>
+                {/* Buttons */}
+                <div className=" flex items-center gap-4 mt-3 mb-3">
+                  <button className=" px-5 animate-pulse min-w-24 rounded py-5 bg-slate-200">
+                    {/* Buy */}
+                  </button>
+                  <button className=" px-5 animate-pulse min-w-24 rounded py-5 bg-slate-200">
+                    {/* Add To Cart */}
+                  </button>
+                </div>
 
-        {/* Description */}
-        <span className=" font-semibold">Description:</span>
-        <p className=" text-justify text-slate-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-          similique quas porro, asperiores doloribus aperiam mollitia dolores
-          aut non, delectus labore ipsum pariatur ipsa exercitationem quisquam
-          magnam libero eligendi sapiente quo sed! Impedit deleniti quo
-          praesentium odio. Minima, corporis. Soluta obcaecati dolorem iusto
-          optio tempora consectetur facilis quaerat officia, dolore libero aut
-          similique excepturi sit adipisci consequuntur mollitia ducimus
-          praesentium deleniti fugit at repellendus quasi. Blanditiis provident,
-          quo perspiciatis totam nobis distinctio eos error amet doloribus ea
-          architecto sit! Corporis iste eligendi ad sint quod! Delectus iusto
-          sint corrupti harum odio laboriosam, itaque eum minima tempora rem
-          suscipit eius aliquid quibusdam reiciendis atque totam quis
-          dignissimos eaque mollitia blanditiis voluptatum, numquam excepturi
-          voluptatibus? Non labore nobis nulla enim? Doloribus, voluptatibus
-          tenetur ad dolorem at unde? Molestias velit asperiores facere
-          consequuntur totam veniam mollitia omnis, aspernatur sunt suscipit,
-          maxime eveniet. Nihil pariatur nostrum rem ut molestiae possimus
-          necessitatibus blanditiis laboriosam iusto quia voluptatum delectus
-          quod perferendis dignissimos ducimus eum non reiciendis provident
-          suscipit ab modi, libero quas maiores! Quos molestiae quibusdam modi
-          veniam dicta suscipit optio dolor impedit eius rerum nemo placeat
-          officia excepturi deserunt recusandae tempore, doloribus cupiditate
-          adipisci et non! Neque ipsum asperiores, ipsam ut ducimus adipisci
-          possimus labore.
-        </p>
+                {/* Description */}
+                <span className=" font-semibold bg-slate-200 px-20 animate-pulse rounded py-1"></span>
+                <p className="bg-slate-200 px-10 py-14 rounded mt-3 animate-pulse"></p>
+              </div>
+            }
+          />
+        ) : (
+          <div className="w-full">
+            {/* Brand Name */}
+            <span className=" bg-red-200 text-red-700 px-2 py-1 rounded-full">
+              {productDetail?.brandName}
+            </span>
+
+            {/* Product name */}
+            <h3 className=" mt-2 text-4xl font-semibold">
+              {productDetail?.productName}
+            </h3>
+
+            {/* Category */}
+            <small className=" text-slate-400 capitalize text-[1rem] mt-3 block">
+              {productDetail?.category}
+            </small>
+
+            {/* Price */}
+            <div className=" flex items-center gap-2">
+              {/* Selling Price */}
+              <h2 className=" text-2xl font-semibold text-red-600">
+                {currencyFormat(productDetail?.sellingPrice)}
+              </h2>
+
+              {/* Original price */}
+              <h2 className=" text-2xl font-semibold line-through text-slate-400">
+                {currencyFormat(productDetail?.price)}
+              </h2>
+            </div>
+
+            {/* Buttons */}
+            <div className=" flex items-center gap-4 mt-3 mb-3">
+              <button className=" px-5 min-w-24 border border-red-600 rounded py-1 text-red-600 hover:bg-red-600 hover:text-white transition-all">
+                Buy
+              </button>
+              <button className=" px-5 min-w-24 bg-red-600 border border-red-600 rounded py-1 text-white hover:bg-white hover:text-red-600 transition-all">
+                Add To Cart
+              </button>
+            </div>
+
+            {/* Description */}
+            <span className=" font-semibold">Description:</span>
+            <p className=" text-justify text-slate-600">
+              {productDetail?.description}
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
