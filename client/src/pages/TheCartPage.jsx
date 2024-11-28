9f74aa4f721e0eb93fb49200bf47d608994da01f
Mushkir <mushkirmohamed@gmail.com>
2024-11-28 19:21:28 +0530

    fix: fixed cart page responsive design bug

--- client/src/pages/TheCartPage.jsx
+++ client/src/pages/TheCartPage.jsx
@@ -46,7 +46,7 @@ const TheCartPage = () => {
   console.log(cartProductDetails);
 
   return (
-    <div className="mt-20 font-Sen container mx-auto px-5">
+    <div className="mt-20 font-Sen container mx-auto px-2 sm:px-5">
       {/* Empty message */}
       <div className=" flex justify-center">
         {countCartItem === 0 && (
@@ -70,7 +70,7 @@ const TheCartPage = () => {
         )}
       </div>
 
-      <div className="flex flex-col md:flex-row md:justify-between gap-5 items-start">
+      <div className="flex flex-col lg:flex-row lg:justify-between gap-5 items-start">
         {/* Products */}
         <div className="w-full max-w-3xl flex flex-col gap-2">
           {countCartItem > 0 && loading ? (
@@ -103,51 +103,51 @@ const TheCartPage = () => {
               return (
                 <div
                   key={product?._id}
-                  className=" bg-white shadow-md rounded overflow-hidden w-full min-h-32 flex"
+                  className=" bg-white shadow-md rounded overflow-hidden w-full h-64 sm:h-32 sm:flex"
                 >
                   {/* Product Img */}
-                  <div className=" w-full max-w-36 h-full bg-slate-200 p-1.5">
+                  <div className="w-full sm:max-w-36 h-32 sm:h-full bg-slate-200 p-1.5">
                     <img
-                      className=" w-full h-full object-cover mix-blend-multiply"
+                      className="w-full h-full object-scale-down sm:object-cover mix-blend-multiply"
                       src={product?.productId?.productImgs[0]}
                       alt=""
                     />
                   </div>
                   {/* Product details */}
-                  <div className="px-5 pt-2 flex flex-col">
-                    <h3 className=" text-2xl font-semibold">
+                  <div className="px-5 pt-2 relative w-full">
+                    {/* Delete icon */}
+                    <div className="absolute right-0 mr-3 mt-1.5 text-red-600 bg-red-50 p-1 hover:bg-red-600 hover:text-white transition-all cursor-pointer rounded-full">
+                      <MdDelete />
+                    </div>
+
+                    <h3 className="lg:text-2xl font-semibold line-clamp-1">
                       {product?.productId?.productName}
                     </h3>
 
                     <span className=" text-slate-400 capitalize text-sm">
                       {product?.productId?.category}
                     </span>
-                    <span className=" text-red-600">
-                      {currencyFormat(product?.productId?.sellingPrice)}
-                    </span>
+
+                    <div className=" flex items-center justify-between gap-3 lg:gap-0">
+                      <span className=" text-red-600 text-sm lg:text-base">
+                        {currencyFormat(product?.productId?.sellingPrice)}
+                      </span>
+
+                      <span className=" text-slate-700 text-sm lg:text-base">
+                        {currencyFormat(1000)}
+                      </span>
+                    </div>
 
                     {/* Buttons */}
                     <div className=" flex items-center gap-3 mt-1.5">
-                      <button className=" border border-red-600 px-2 rounded text-red-600 hover:bg-red-600 hover:text-white transition-all">
+                      <button className=" border flex items-center justify-center border-red-600 w-6 h-6 rounded text-red-600 hover:bg-red-600 hover:text-white transition-all">
                         -
                       </button>
                       <span>{product?.qty}</span>
-                      <button className=" border border-red-600 px-2 rounded text-red-600 hover:bg-red-600 hover:text-white transition-all">
+                      <button className=" border flex items-center justify-center border-red-600 w-6 h-6 rounded text-red-600 hover:bg-red-600 hover:text-white transition-all">
                         +
                       </button>
                     </div>
-                  </div>
-
-                  {/* Total Price  */}
-                  <div className=" ml-auto flex flex-col items-end justify-evenly pr-3">
-                    {/* Delete Icon */}
-                    <div className="text-red-600 bg-red-50 p-1 hover:bg-red-600 hover:text-white transition-all cursor-pointer rounded-full">
-                      <MdDelete />
-                    </div>
-
-                    <span className=" text-base font-semibold">
-                      {currencyFormat(1000)}
-                    </span>
                   </div>
                 </div>
               );
@@ -156,7 +156,25 @@ const TheCartPage = () => {
         </div>
 
         {/* Total */}
-        <div className=" bg-yellow-500 p-5 w-full md:max-w-sm"></div>
+        <div className=" bg-white w-full md:max-w-sm rounded overflow-hidden">
+          <h3 className="px-5 p-2 bg-red-600 text-white">Summary</h3>
+
+          <div className=" px-5 py-2">
+            <div className=" flex items-center justify-between mb-2">
+              <span>Quantity</span>
+              <span>6</span>
+            </div>
+
+            <div className=" flex items-center justify-between mb-2">
+              <span>Total Price</span>
+              <span>{currencyFormat(1000)}</span>
+            </div>
+          </div>
+
+          <button className=" bg-blue-500 w-full px-5 py-2 hover:bg-blue-600 text-white transition-all">
+            Payment
+          </button>
+        </div>
       </div>
     </div>
   );
