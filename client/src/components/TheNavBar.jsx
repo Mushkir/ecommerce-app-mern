d90d0c0c0950cf898779d0921fe806a5cdf5e68f
Mushkir <mushkirmohamed@gmail.com>
2024-11-29 22:07:38 +0530

    fix: fixed cart icon bug in navbar

--- client/src/components/TheNavBar.jsx
+++ client/src/components/TheNavBar.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { UserLogout } from "../redux/user/userSlice";
 import apiEndPointObj from "../common/api_uri";
 import Context from "../context/context";
+import { GiShoppingCart } from "react-icons/gi";
+import { BsCartFill } from "react-icons/bs";
 
 const TheNavBar = () => {
   const navigate = useNavigate();
@@ -97,25 +99,17 @@ const TheNavBar = () => {
           </div>
 
           {/* Cart */}
-          {countCartItem > 0 && currentUser?.currentUser?._id && (
+          {currentUser?.currentUser?._id && (
             <Link to={"/cart"} className="relative cursor-pointer">
-              <div className="text-black">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="25"
-                  height="25"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2s-.9-2-2-2M1 2v2h2l3.6 7.59l-1.35 2.45c-.16.28-.25.61-.25.96c0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12l.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49A1.003 1.003 0 0 0 20 4H5.21l-.94-2zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2s2-.9 2-2s-.9-2-2-2"
-                  />
-                </svg>
+              <div className="text-black text-2xl">
+                {countCartItem > 0 ? <BsCartFill /> : <GiShoppingCart />}
               </div>
 
-              <span className="text-sm absolute flex items-center bg-red-500 w-5 h-5 text-white p-1 rounded-full -top-3 -right-3">
-                {countCartItem}
-              </span>
+              {countCartItem > 0 && (
+                <span className="text-sm absolute flex items-center bg-red-500 w-5 h-5 text-white p-1 rounded-full -top-3 -right-3">
+                  {countCartItem}
+                </span>
+              )}
             </Link>
           )}
 
